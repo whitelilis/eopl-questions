56ee45fb4ab9a1433be34727e58e72d8bba5c588
whitelilis <liu.zhe@admaster.com.cn>
2015-10-07 23:36:57 +0800

    solve 1.8 and 1.9

--- ch1.lisp
+++ ch1.lisp
@@ -12,3 +12,21 @@
               (car _lst)
               (nth-helper (cdr _lst) (- _n 1) _lst_raw _n_raw)))))
   (nth-helper lst n lst n))
+
+
+;;; 1.8 In the definition of remove-first, if the last line were replaced by (remove-first s (cdr los)), what function would the resulting procedure compute? Give the contract, including the usage statement, for the revised procedure.
+
+;;  remove-until-first : Sym × Listof(Sym) → Listof(Sym)
+;;  (remove-until-first s los) removing from beginning until first occurrence of the symbol s, then remove s, return others with the same elements arranged in the same order as los.
+
+
+;;; 1.9 Define remove, which is like remove-first, except that it removes all occurrences of a given symbol from a list of symbols, not just the first.
+;; remove is defined in cl, so define remove-wizard
+
+(defun remove-wizard (s los)
+  (if (null los)
+      los
+      (progn
+        (if (eql s (car los))
+            (remove-wizard s (cdr los))
+            (cons (car los) (remove-wizard s (cdr los)))))))
